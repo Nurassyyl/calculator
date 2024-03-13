76dab919be2d63a58764033e860fb231befdb255
Nurassyl Abdildabek <abdildabekn@mail.ru>
2024-03-13 13:15:06 +0500

    Доработал ползунок

--- toUpCard.js
+++ toUpCard.js
@@ -4,6 +4,23 @@ const addMoneySum = document.querySelectorAll('.add-money-sum');
 const sum = document.querySelector('.svelte-br80q');
 const sizePic = document.getElementById('svelte-1dvvwkv');
 
+sizePic.oninput = function () {
+  let price = this.value;
+  if (price <= 1000) {
+    pricePercent = (price / 100) * 50 + (price / 1);
+    sum.textContent = pricePercent + " ₽";
+  } else if (price > 1000 && price <= 3000) {
+    pricePercent = (price / 100) * 40 + (price / 1);
+    sum.textContent = pricePercent + " ₽";
+  } else if (price > 3000 && price <= 5000) {
+    pricePercent = (price / 100) * 35 + (price / 1);
+    sum.textContent = pricePercent + " ₽";
+  } else {
+    pricePercent = (price / 100) * 30 + (price / 1);
+    sum.textContent = pricePercent + " ₽";
+  }
+}
+
 inputNumber.addEventListener('input', (e) => {
   let price = e.target.value;
   if (price <= 1000) {
@@ -28,6 +45,7 @@ inputNumber.addEventListener('input', (e) => {
   // const card = { title, pricePercent };
   // localStorage.setItem("cart", JSON.stringify([...cart, card]));
 })
+
 
 addMoney.forEach(el => {
   el.addEventListener('click', (e) => {
